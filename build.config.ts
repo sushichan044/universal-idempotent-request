e0058a9e0b1e63605331b371db80be80becb52b3
sushichan044 <71284054+sushichan044@users.noreply.github.com>
2025-03-21 18:03:56 +0900

    build: export strategy

--- build.config.ts
+++ build.config.ts
@@ -7,6 +7,7 @@ export default defineBuildConfig({
     "src/brand/index.ts",
     "src/server-specification/index.ts",
     "src/storage/index.ts",
+    "src/strategy.ts",
   ],
   outDir: "dist",
 });
