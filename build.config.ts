610a09cf9aacaa44dcf27f1f4cdf7f52b9b03035
sushichan044 <71284054+sushichan044@users.noreply.github.com>
2025-03-21 14:07:57 +0900

    build: export server-specification, storage interface

--- build.config.ts
+++ build.config.ts
@@ -2,6 +2,11 @@ import { defineBuildConfig } from "unbuild";
 
 export default defineBuildConfig({
   declaration: true,
-  entries: ["src/index.ts", "src/brand/index.ts"],
+  entries: [
+    "src/index.ts",
+    "src/brand/index.ts",
+    "src/server-specification/index.ts",
+    "src/storage/index.ts",
+  ],
   outDir: "dist",
 });
