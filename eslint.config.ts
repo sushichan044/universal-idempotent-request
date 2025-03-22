b96b34889f7bec297dbc8c54264c02e51eb5ee06
sushichan044 <71284054+sushichan044@users.noreply.github.com>
2025-03-22 23:28:14 +0900

    chore: inject global builtin for unicorn

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,11 +1,15 @@
 import ts from "@virtual-live-lab/eslint-config/presets/ts";
 import eslintPluginUnicorn from "eslint-plugin-unicorn";
+import globals from "globals";
 import tseslint from "typescript-eslint";
 
 export default tseslint.config(
   ts,
   {
     extends: [eslintPluginUnicorn.configs.recommended],
+    languageOptions: {
+      globals: globals.builtin,
+    },
     name: "@repo/eslint-config/unicorn",
     rules: {
       "unicorn/custom-error-definition": "error",
