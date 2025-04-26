ccd8ef00c318aa2ec9d5e7ac2f2d7a6151b640ae
sushichan044 <71284054+sushichan044@users.noreply.github.com>
2025-04-26 20:27:33 +0900

    fix: sunset should be calculated on every save

--- examples/cloudflare-kv-storage/src/storage.ts
+++ examples/cloudflare-kv-storage/src/storage.ts
@@ -12,13 +12,14 @@ export const createCloudflareKVStorageDriver = (
   kv: KVNamespace,
 ): IdempotentRequestStorageDriver => {
   const TTL_ONE_HOUR = 60 * 60;
-  const sunset = getExpirationEpoch(TTL_ONE_HOUR);
 
   return {
     async get(storageKey) {
       return await kv.get<IdempotentRequest>(storageKey, "json");
     },
     async save(request) {
+      const sunset = getExpirationEpoch(TTL_ONE_HOUR);
+
       await kv.put(request.storageKey, JSON.stringify(request), {
         expiration: sunset,
       });
